854766fd210350d3af83c252d16c4bed9e35e131
Laurentius Herigaudus <90922016+Herigaudus@users.noreply.github.com>
2024-07-21 19:17:08 +0200

    rollback no load more

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('pluginData', () => ({
         plugins: [],
-        displayedPlugins: [],
         selectedCategories: [],
         selectedPaid: '',
         selectedVersions: [],
-        currentIndex: 0,
-        itemsPerPage: 10,
-        
+
         fetchPlugins() {
             fetch('data/links.json')
                 .then(response => {
@@ -19,22 +16,12 @@ document.addEventListener('alpine:init', () => {
                 .then(data => {
                     console.log('Plugins fetched:', data); // Debugging line
                     this.plugins = data;
-                    this.loadMorePlugins(); // Load initial set of plugins
                 })
                 .catch(error => {
                     console.error('Fetch error:', error);
                 });
         },
-        
-        loadMorePlugins() {
-            if (this.currentIndex < this.plugins.length) {
-                const newPlugins = this.plugins.slice(this.currentIndex, this.currentIndex + this.itemsPerPage);
-                this.displayedPlugins = [...this.displayedPlugins, ...newPlugins];
-                this.currentIndex += this.itemsPerPage;
-                console.log('Plugins displayed:', this.displayedPlugins); // Debugging line
-            }
-        },
-        
+
         get uniqueCategories() {
             const categories = new Set();
             this.plugins.forEach(plugin => {
@@ -42,17 +29,19 @@ document.addEventListener('alpine:init', () => {
             });
             return Array.from(categories);
         },
-        
+
         get uniqueVersions() {
             const versions = new Set();
             this.plugins.forEach(plugin => {
-                versions.add(plugin.version);
+                if (plugin.version) { // Check if version exists
+                    versions.add(plugin.version);
+                }
             });
             return Array.from(versions);
         },
-        
+
         get filteredPlugins() {
-            return this.displayedPlugins.filter(plugin => {
+            return this.plugins.filter(plugin => {
                 const matchesCategory = this.selectedCategories.length === 0 || this.selectedCategories.every(category => plugin.categories.includes(category));
                 const matchesPaid = this.selectedPaid === '' || plugin.paid.toString() === this.selectedPaid;
                 const matchesVersion = this.selectedVersions.length === 0 || this.selectedVersions.includes(plugin.version);
